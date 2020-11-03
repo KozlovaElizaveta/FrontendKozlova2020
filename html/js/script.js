be2710de9c6d1ea9e0da886d1f2abee9dec6017a
Liza <kozlovaggo@gmail.com>
2020-11-03 17:43:02 +0400

    3.2 js

--- html/js/script.js
+++ html/js/script.js
@@ -1,22 +1,10 @@
 let counter = 0
 
-    function add_to_cart(){
-        if (counter != 0) {
+      function add_to_cart(){
           counter++;
           let k = document.getElementById("cart");
           k.innerHTML = counter;
-        }
-        else{
-          counter++;
-          let k = document.getElementById("cart");
-          if (counter == 1) {
-            k.classList.remove("hide");
-            k.classList.add("show");
-          }
-        }
       }
-
-
 
 
       
