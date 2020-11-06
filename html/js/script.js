f8a3688f7f66b9dd8af6241f00381b2159a9734b
Liza <kozlovaggo@gmail.com>
2020-11-06 17:52:50 +0400

    popup popup...

--- html/js/script.js
+++ html/js/script.js
@@ -7,5 +7,57 @@ let counter = 0
       }
 
 
-      
+/*
+var counter = 0;
+var basketElement = document.getElementById('basket');
+var itemsList = document.getElementById('itemsList');
+var popup = document.getElementById('popup');
+
+var items = [];
+
+function addItemToCart(title, price, id) {   
+  counter++;
+  basketElement.innerText = counter;
+  
+  let isItemExist = false;
+  
+  for (let i=0; i< items; i++) {
+  }
+  if (isItemExist) {
+  } else {
+    items.push({
+      title: title,
+      price: price,
+      id: id,
+      count: 1  
+    });
+  }
+  
+  updatePopupData();
+}
+
+
+function updatePopupData() {
+  for(let i = 0; i < items.length; i++) {
+    basketItem.classList.add('basket-item'); 
+    basketItem.innerText = items[i].title;  
+  }
+}
+
+function openPopup() {
+  popup.style.visibility = 'visible';
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
 
